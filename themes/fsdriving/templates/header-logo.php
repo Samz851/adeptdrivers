8f4a934813fa30b91b2d10371a33276087ed7b83
Samer Alotaibi <sam.otb@hotmail.ca>
2020-11-08 22:42:32 -0500

    theme fix original

--- themes/fsdriving/templates/header-logo.php
+++ themes/fsdriving/templates/header-logo.php
@@ -17,7 +17,7 @@ if ((fsdriving_get_theme_option('header_style') == 'header-default') && (!empty(
 	?><a class="sc_layouts_logo" href="<?php echo is_front_page() ? '#' : esc_url(home_url('/')); ?>"><?php
 		if (!empty($fsdriving_logo_image)) {
 			$fsdriving_attr = fsdriving_getimagesize($fsdriving_logo_image);
-			echo '<img src="'.esc_url($fsdriving_logo_image).'" alt="'.esc_arrt(basename($fsdriving_logo_image)).'"'.(!empty($fsdriving_attr[3]) ? sprintf(' %s', $fsdriving_attr[3]) : '').'>' ;
+			echo '<img src="'.esc_url($fsdriving_logo_image).'" alt="'.esc_attr(basename($fsdriving_logo_image)).'"'.(!empty($fsdriving_attr[3]) ? sprintf(' %s', $fsdriving_attr[3]) : '').'>' ;
 		} else {
 			fsdriving_show_layout(fsdriving_prepare_macros($fsdriving_logo_text), '<span class="logo_text">', '</span>');
 			fsdriving_show_layout(fsdriving_prepare_macros($fsdriving_logo_slogan), '<span class="logo_slogan">', '</span>');
