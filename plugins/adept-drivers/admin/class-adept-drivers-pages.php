847f06dea95174f5e4f14a63235a7de50783c0a3
Samer Alotaibi <sam.otb@hotmail.ca>
2020-11-13 23:03:08 -0500

    generating zcrm tokens

--- plugins/adept-drivers/admin/class-adept-drivers-pages.php
+++ plugins/adept-drivers/admin/class-adept-drivers-pages.php
@@ -276,7 +276,7 @@ class Adept_Drivers_Pages {
 		?>
 		<input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ad_options[<?php echo esc_attr( $args['label_for'] ); ?>]" 
 		value="<?php echo isset( $options[ $args['label_for'] ] ) ? $options[ $args['label_for'] ] : '' ; ?>" />
-		<button id="generate_token"><?php __('Generate Access Tokens', 'adept-drivers'); ?></button>
+		<button id="generate_token"><?php echo __('Generate Access Tokens', 'adept-drivers'); ?></button>
 
 		<?php
 	}
